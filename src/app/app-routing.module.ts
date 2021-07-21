6d7bace506c49349c6a8d2a248cc3e6ae28df92a
mulsannestraight <22200664+mulsannestraight@users.noreply.github.com>
2021-07-21 00:43:47 -0400

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
+import {LoginComponent} from "./login/login.component";
+import {EventManagementComponent} from "./event-management/event-management.component";
+import {UserEventManagementComponent} from "./user-event-management/user-event-management.component";
+import {LoginGuard} from "./guards/login.guard";
+import {AdminGuard} from "./guards/admin.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  {
+    path: "event_mgmt",
+    component: EventManagementComponent,
+    canActivate: [AdminGuard]
+  },
+  {
+    path: "user_event_mgmt",
+    component: UserEventManagementComponent,
+    canActivate: [LoginGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {MatDialogModule} from "@angular/material/dialog";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { EventManagementComponent } from './event-management/event-management.component';
+import { UserEventManagementComponent } from './user-event-management/user-event-management.component';
+import { AddEventComponent } from './add-event/add-event.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EventManagementComponent,
+    UserEventManagementComponent,
+    AddEventComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
